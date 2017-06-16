d854d5ce7a9444cafc5b118e9722a9e17e40bc1d
Rathi <github email address>
2017-06-16 10:06:09 -0400

    did it

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,33 @@
 class Triangle
-  # write code here
+  attr_accessor :s1, :s2, :s3
+
+  def initialize(a,b,c)
+    @s1 = a
+    @s2 = b
+    @s3 = c
+  end
+
+  def kind
+    validates_side
+    if s1 == s2 && s2 == s3
+      :equilateral
+    elsif s1 == s2 || s2 == s3 || s1 == s3
+      :isosceles
+    else
+      :scalene
+    end
+end
+    def validates_side
+      real_triangle = [(s1 + s2 > s3), (s1 + s3 > s2), (s2 + s3 > s1)]
+      [s1, s2, s3].each { |s| real_triangle << false if s <= 0 }
+      raise TriangleError if real_triangle.include?(false)
+    end
+  end
+
+
+
+class TriangleError < StandardError
+  def message
+    "This is not a Triangle"
+  end
 end
